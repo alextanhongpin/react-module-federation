069aa0a7ea54c47b67a0487362526cd50afc64b6
alextanhongpin <alextan220990@gmail.com>
2023-10-12 00:46:22 +0800

    chore: add module federation

--- home-app/src/App.js
+++ home-app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from "react"; // Must be imported for webpack to work
+import "./App.css";
+
+const Header = lazy(() => import("HeaderApp/Header"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<div>Loading Header...</div>}>
+        <Header />
+      </Suspense>
+      <div>My app</div>
     </div>
   );
 }
